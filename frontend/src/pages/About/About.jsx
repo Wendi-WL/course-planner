e67e854336ee09913046edec1c746e645c31864a
Crystalstar05 <54647199+Crystalstar05@users.noreply.github.com>
2025-06-03 16:39:12 -0700

    Made first description of About

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,5 +1,13 @@
 function About() {
-  return <div className="Page-header">About</div>;
+  return (
+    <div className="Page">
+      <div className="Page-header">About</div>
+      <p>UBCCoursePlanner is meant to help students pick courses and electives for their degree. It offers 2 tools: </p>
+      <p>1. Degree Tracker aims to help students ... </p>
+      <p>2. Eligibility Tool aims to help students ... </p>
+      <p>Disclaimer: UBCCoursePlanner is not affiliated with the University of British Columbia. UBC Course Planner is not responsible for...</p>
+    </div>
+  )
 }
 
 export default About;
