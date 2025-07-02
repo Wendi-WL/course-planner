a334c71d323a47a6311e809e804bd078ecb740b8
llivvv <livialin2014@gmail.com>
2025-07-01 20:52:57 -0700

    made basic page structure

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,4 +1,21 @@
+import "./Contact.css";
+
 function Contact() {
-  return <div>Contact</div>;
+  // const theme = useTheme();
+  // const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <div className='Page'>
+      <div className='title-desc'>
+        <h1>Contact Us</h1>
+        <p className='desc'>
+          We'd love to answer any questions and hear any feedback you may have.
+        </p>
+      </div>
+      <section className='main-contact'>
+        <div className='form-container'></div>
+      </section>
+    </div>
+  );
 }
 export default Contact;
