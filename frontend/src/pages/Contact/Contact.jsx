dac7f926c997c79025e721a49a25593ace6332fd
llivvv <livialin2014@gmail.com>
2025-07-11 15:24:22 -0700

    added send functionality

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,8 +2,36 @@ import Button from "../../components/ui/Button/Button";
 import Footer from "../../components/ui/Footer/Footer";
 import "./Contact.css";
 import TextField from "@mui/material/TextField";
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const sendEmail = ({ yourName, yourEmail, specSubject, msg }) => {
+    const templateParams = {
+      from_name: yourName,
+      from_email: yourEmail,
+      subject: specSubject,
+      message: msg,
+    };
+    console.log(templateParams);
+
+    emailjs
+      .send("service_asumvkd", "template_kf8b4zr", templateParams, {
+        publicKey: "2s3M43dKJ7I3v2XDT",
+      })
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   // styles for the form
   const fieldStyle = {
     zIndex: "1",
@@ -20,49 +48,70 @@ function Contact() {
         </p>
       </div>
       <section className='main-contact'>
-        <div className='form-container'>
-          <TextField
-            fullWidth
-            label='Your Name'
-            variant='outlined'
-            placeholder='Enter your name'
-            size='small'
-            slotProps={shrinkLabel}
-            sx={fieldStyle}
-          />
-          <TextField
-            required
-            fullWidth
-            label='Your email'
-            variant='outlined'
-            placeholder='Enter your email'
-            size='small'
-            slotProps={shrinkLabel}
-            sx={fieldStyle}
-          />
-          <TextField
-            required
-            fullWidth
-            label='Subject'
-            variant='outlined'
-            placeholder='Enter topic of message'
-            size='small'
-            slotProps={shrinkLabel}
-            sx={fieldStyle}
-          />
-          <TextField
-            className='msg-input'
-            required
-            fullWidth
-            label='Message'
-            multiline // textarea element rendered instead of input
-            rows={5}
-            placeholder='Type your questions/feedback here'
-            slotProps={shrinkLabel}
-            // sx={fieldStyle}
-          />
-          <Button text='Send' />
-        </div>
+        <form onSubmit={handleSubmit(sendEmail)}>
+          <div className='form-container'>
+            <TextField
+              fullWidth
+              name='yourName'
+              label='Your Name'
+              variant='outlined'
+              placeholder='Enter your name'
+              size='small'
+              slotProps={shrinkLabel}
+              sx={fieldStyle}
+              {...register("yourName")}
+            />
+            <TextField
+              fullWidth
+              required
+              name='email'
+              label='Your email'
+              variant='outlined'
+              placeholder='Enter your email'
+              size='small'
+              slotProps={shrinkLabel}
+              sx={fieldStyle}
+              type='email'
+              {...register("yourEmail", {
+                required: "Email is required",
+              })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+            <TextField
+              fullWidth
+              name='subject'
+              label='Subject'
+              variant='outlined'
+              placeholder='Enter topic of message'
+              size='small'
+              slotProps={shrinkLabel}
+              sx={fieldStyle}
+              {...register("specSubject")}
+            />
+            <TextField
+              className='msg-input'
+              required
+              fullWidth
+              name='message'
+              label='Message'
+              multiline // textarea element rendered instead of input
+              rows={5}
+              placeholder='Type your questions/feedback here'
+              slotProps={shrinkLabel}
+              {...register("msg", {
+                required: "Message is required",
+              })}
+              error={!!errors.message}
+              helperText={errors.message?.message}
+              // sx={fieldStyle}
+            />
+            <Button
+              type='submit'
+              text='Send'
+            />
+          </div>
+        </form>
       </section>
       <Footer />
     </div>
