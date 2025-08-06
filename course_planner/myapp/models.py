02a6ef4defd2a17996ae43f7f05b355649db8a6f
Wendi <liu.wendi@yahoo.ca>
2025-08-06 14:26:18 -0700

    create find eligible courses function and tests

--- course_planner/myapp/models.py
+++ course_planner/myapp/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Course(models.Model) :
-    subject = models.CharField(max_length=4)
-    code = models.IntegerField(min=100, max=699)
-    name = models.CharField(max_length=100)
-    credits = models.PositiveSmallIntegerField
-    cdf = models.BooleanField     #credit/d/failing available?
-    desc = models.TextField
-    prereqs = models.ManyToManyField("self", symmetrical=False)
-
-
-
-
