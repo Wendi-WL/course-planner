79e0aa695dca073c3507b6c55945a7cff93bda0f
llivvv <livialin2014@gmail.com>
2025-08-08 16:32:12 -0700

    add email validation

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -102,6 +102,10 @@ function Contact() {
               type='email'
               {...register("yourEmail", {
                 required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
               })}
               error={!!errors.yourEmail}
             />
